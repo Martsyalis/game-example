de8cb51a4711c3acb4530fa754be66b5a4246f77
Maryus Martsyalis <martsyalis@gmail.com>
2019-09-22 19:06:59 -0700

    add Operative Clss

---
+++ Operative.js
@@ -0,0 +1,28 @@
+class Operative {
+  constructor(
+    name,
+    hp,
+    exp,
+    weapon,
+    tactical,
+    marksmanship,
+    cover,
+    ambush,
+    stealth
+  ) {
+    (this.name = name),
+      (this.hp = hp),
+      (this.exp = exp),
+      (this.weapon = weapon),
+      (this.skills = {
+        tactical,
+        marksmanship,
+        cover,
+        ambush,
+        stealth
+      }),
+      (this.upgradeSkill = function(skill, points) {
+        this.skills[skill] += points;
+      });
+  }
+}
