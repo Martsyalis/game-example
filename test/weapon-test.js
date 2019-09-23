c52bcb143be6e35ea64f0dc57b2fa4f6f2facf80
Maryus Martsyalis <martsyalis@gmail.com>
2019-09-22 19:57:29 -0700

    refactors tests

--- test/weapon-test.js
+++ test/weapon-test.js
@@ -1,10 +1,22 @@
 import assert from "assert";
 import Weapon from "../classes/Weapon";
 
-const uzi = new Weapon("uzi", 10, 1);
+const uzi = new Weapon("uzi", 10, 1, 4);
 describe("Weapon assault test", function() {
-  it("performs an attack at range", function() {
+  it("performs an attack at perfect range", function() {
     const dmg = uzi.dmgAtRange(1);
     assert.equal(dmg, 10);
+  });
+  it("performs an attack at range thats off by 1", function() {
+    const dmg = uzi.dmgAtRange(2);
+    assert.equal(dmg, 6);
+  });
+  it("performs an attack at range thats off by 2", function() {
+    const dmg = uzi.dmgAtRange(3);
+    assert.equal(dmg, 2);
+  });
+  it("performs an attack at range thats off by 3", function() {
+    const dmg = uzi.dmgAtRange(4);
+    assert.equal(dmg, 0);
   });
 });
